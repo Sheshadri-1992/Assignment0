f55b476a54d3ed2187b273bc2922e03e3cea6681
Sheshadri-1992 <sheshadri.madhu@gmail.com>
2019-01-19 22:32:07 +0530

    Plots and References added

--- plots/plot.py
+++ plots/plot.py
@@ -6,7 +6,7 @@ import matplotlib.patches as mpatches
 import matplotlib.colors as colors
 import numpy as np
 
-fname="/home/swamiji/phd/sem2/ssds/Assignment0/plots/buckets.txt"
+fname="/home/swamiji/phd/sem2/ssds/Assignment0/plots/values.txt"
 
 
 def plotGraph(content):
@@ -14,23 +14,34 @@ def plotGraph(content):
 	plt.subplot(111)
 	fig, ax1 = plt.subplots()	
 
-	X = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47]
-	Y = [174584965,26061816,7962843,3563603,1875683,1038112,560681,306767,225191,144416,97322,68856,48926,32184,18763,9015,4954,2609,1386,1393,1048,687,391,369,188,131,23,12,40,15,4,5,2,1,0,1,0,0,0,0,0,0,0,1,0,0,0,1]
+	X = []
+
+	with open("buckets.txt") as f:
+		X = f.readlines()
+
+	X = [a.strip() for a in X]
+
+	Y = []
+
+	with open("values.txt") as f:
+		Y = f.readlines()
+
+	Y = [float(a.strip()) for a in Y]
+	
 	
 
-	plt.bar(X , Y, width = 1,color = 'skyblue')	
+	# plt.bar(X , Y, width = 1,color = 'skyblue')	
+	plt.plot(X,Y)	
+	plt.xlabel("Buckets (hashtags per tweet) ",fontsize =14)
 
-	plt.xlabel("Buckets (hashtags per tweet) ",fontsize =18)
-	plt.xticks(X,X,fontsize=12, rotation=45)
-	plt.ylabel("Number of Users",fontsize=18)	    
-	plt.title('Frequency of Users vs Number of Users ', fontsize=16)	
-	plt.rc('axes',titlesize=18)
-	plt.rc('axes',labelsize=18)	
+	plt.xticks(X,X,fontsize=8, rotation=30,weight='bold')
+	plt.yticks(weight='bold')
+	plt.ylabel("Number of Users",fontsize=14)	    
+	plt.title('Frequency of Users vs Number of Users ', fontsize=14)	
+	plt.rc('axes',titlesize=18)	
 	plt.rc('legend', fontsize=18)    # legend fontsize
 	plt.rc('figure', titlesize=18)	
-	plt.ylim([0,174584965])
-	plt.tight_layout()
-	
+
 	#save the
 	plt.savefig('graph.png')
 
